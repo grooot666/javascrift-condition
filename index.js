c90c1772530b57a8e6b956ea27d6523bbb523f01
grooot666 <Bayarjargal5337@gmail.com>
2024-12-05 14:54:40 +0800

    exercise

---
+++ index.js
@@ -0,0 +1,467 @@
+console.log("hello");
+
+// // data types =>
+// // text => string => "  hello  "
+// // number => 1 4 67 124
+// // boolean => true  || false
+// // float => 0.6
+
+// // variable  =>  store information => data types
+
+// // const myName = "elbeg";
+// // console.log(myName);
+
+// // myName = "bat";
+// // console.log(myName);
+// // let age = 10;
+// // console.log(age);
+// // age = true;
+
+// // console.log(age);
+
+// // operations => + , -, %, * , < , > , <= >=, && , ||
+
+// // const a = 10;
+// // const b = 1;
+// // const c = 7;
+
+// // const sumNUmber = a + b + c;
+
+// // Condition
+
+// // const name = "bat";
+
+// // if (name === "bat") {
+// //   console.log("this is bat");
+// // } else if (name === "Elbeg") {
+// //   console.log("this is elbeg");
+// // } else {
+// //   console.log("this is no one");
+// // }
+
+
+
+// // Exercise 1
+// // <h3>Write a  which checks given input/parameter:</h3>
+// // <ul>
+// //   <li>If input/parameter is divisible by 3 print => Fizz</li>
+// //   <li>If input/parameter is divisible by 5 print => Buzz</li>
+// //   <li>If input/parameter is divisible by 3 or 5 print => FizzBuzz</li>
+// //   <li>If input/parameter is NOT divisible by 3 or 5 print => given Input Number/Value</li>
+// //   <li>If input/parameter is other than Number/Value print => 'Nan - Not a Number! Please Input Number' </li>
+// // </ul>
+
+ 
+ 
+// // Exercise 2
+    
+// // Write a JavaScript conditional statement to find the largest of five numbers.
+// // check input numbers are the type of number, if not console.log('Invalid inputs
+ 
+// // Sample numbers : -5, -2, -6, 0, -1
+// // Output : 0
+ 
+// // Exercise 3
+ 
+// // Write a JavaScript program that accept two integers and display the larger.
+ 
+// // check input numbers are the type of number, if not console.log('Invalid inputs)
+ 
+// // Sample numbers : 2,5
+// // Output : 5
+ 
+// // Exercise 4
+ 
+// // Write a JavaScript conditional statement to find the sign of product of three numbers.
+// // Display an console.log box with the specified sign.
+ 
+// // Sample numbers : 3, -7, 2
+// // Output : The sign is -
+ 
+// // Exercise 5
+ 
+// // Choose the correct comparison operator to console.log true, when x is greater than y.
+// // Choose the correct comparison operator to console.log false, when x is equal to y.
+// // Choose the correct comparison operator to console.log not sure, when x is NOT equal to y.
+ 
+// // Exercise 6
+ 
+// // The Grade Assigner
+// // - Write a if/else statement that:
+// //   - takes 1 variable, a number score.
+// //   - returns a grade for the score, either "A", "B", "C", "D", or "F".
+// // - Call that if/else statement for a few different scores and log the result to make sure it works.
+ 
+// // Exercise 10 j
+// // Write If statement that takes string
+// //     if string length is more than 10 print the string  has more than 10 characters
+// //      if string length is more than 5 return the string has more than 5 characters
+// //       if string length is less than 1 return the string has nothing
+// //       if string length is equal to 1 return the string has 1 character
+// // Exercise 7 j
+ 
+// //  3) Write conditional expressions to satisfy the following safety rules:
+ 
+// //  a) If crewStatus is true, print "Crew Ready" else print "Crew Not Ready".
+ 
+// //  b) If computerStatusCode is 200, print "Please stand by. Computer is rebooting." Else if computerStatusCode is 400, print "Success! Computer online." Else print "ALERT: Computer offline!"
+ 
+// //  c) If shuttleSpeed is > 17,500, print "ALERT: Escape velocity reached!" Else if shuttleSpeed is < 8000, print "ALERT: Cannot maintain orbit!" Else print "Stable speed".
+ 
+// // Write If statement that takes number checks if the number is odd or even
+ 
+// //     if odd return "the number is odd"
+// //     if even return "the number is even"
+ 
+// // Exercise 8 j
+ 
+// // Write if statement that takes role and return each role with greeting
+ 
+// //     if role is 'Employee' return 'Hello'
+// //     if role is 'Director' return 'Greetings'
+// //     if role is '' return 'Please provide role'
+// //     else return 'Hi'
+ 
+// // Exercise 8 j
+ 
+// // 0. Гончигсумлай хоолны газар оров. Орсон хоолны газар нь 5000₮ - 30000₮ хооронд 15% нэмж төлдөг,
+// // бусад үед 20% - ийг нэмж төлдөг бол. Эдгээр тохиолдолуудад нийт хэдийг төлөхийг олно уу.
+// // Эдгээр тохиолдолуудад тус бүр хэдийг төлөх вэ? 3000₮, 27500₮, 100000₮.
+
+
+
+
+
+ 
+// // Exercise 1
+// // <h3>Write a  which checks given input/parameter:</h3>
+// // <ul>
+// //   <li>If input/parameter is divisible by 3 print => Fizz</li>
+// //   <li>If input/parameter is divisible by 5 print => Buzz</li>
+// //   <li>If input/parameter is divisible by 3 or 5 print => FizzBuzz</li>
+// //   <li>If input/parameter is NOT divisible by 3 or 5 print => given Input Number/Value</li>
+// //   <li>If input/parameter is other than Number/Value print => 'Nan - Not a Number! Please Input Number' </li>
+// // </ul>
+
+// // const giveNumber = "k"
+// const giveNumber = prompt("Enter a number")
+
+
+
+// // if ( giveNumber % 3 == 0 || giveNumber % 5 == 0){
+// //     console.log("fizz and buzz")
+// // }else if (giveNumber % 3 == 0)  { 
+// //     console.log ("fizz");
+// // }
+// // else if (giveNumber % 5 == 0){
+// //     console.log("buzz");
+// // }
+// // else if( typeof giveNumber !=="number"){
+// //     console.log("Nan - Not a Number")
+// // }
+
+
+// // else if (giveNumber %3 !== 0 || giveNumber % 5 !== 0 )
+// // {
+// //     console.log("grot")
+// // }
+
+
+// // Exercise 2
+    
+// // Write a JavaScript conditional statement to find the largest of five numbers.
+// // check input numbers are the type of number, if not console.log('Invalid inputs
+ 
+// // Sample numbers : -5, -2, -6, 0, -1
+// // Output : 0
+
+// // let => dotor utgaa solij bolno , const => dotor utgaa solij bolohgui  , var => buh soilj uyn hatan 
+
+// //   key : value 
+// // syntax ==> herhen code bichigdej bgaa 
+//  // ==> variable 
+
+// var a=-5
+// var b=-2
+// var c=-6
+// var d=0
+// var e=-1
+
+// // Data Types
+// // string    => ""  ==> "hi dear"  ==> "5"
+// // number  => 5,  -2 , 2.3 ==> NaN (not a number)
+// // boolean ==> true, false 
+// // null ==> empty
+// //  undefined => orshidoggui 
+// // array =[]
+// // object ={}
+
+// // IF condition nuhtsul unen hudlaa yaahiin ymar uildel hiihiin be ?
+// //  Opertors 
+// // <, > , <=, >=, -, +
+// // logic opertors 
+// //  && ==> and (hoyulaa unen ), || ==> (ali neg ni unen baih) , NOR  ! (esregeerei)
+
+
+
+
+// if (typeof numberOne !== "number" && typeof numberTwo !== "number" && typeof numberTree  !== "number" && typeof numberfour !== "number" &&  typeof numberfive !== "number")
+//     {
+//     console.log("Invalid input");
+// }
+
+//  if (a>b && a>c && a>d && a>e){
+//     console.log(a);
+   
+//  }else if (b>a && b>c && b>d && b>e){
+//     console.log(b);
+    
+//  }else if (c>a && c>b && c>d && c>e){
+//     console.log(c);
+    
+//  }else if (d>a && d>b && d>c && d>e){
+//     console.log(d);
+    
+//  }else if (e>a && e>b && e>c && e>d){
+//     console.log(e);
+    
+//  }
+
+//  // Exercise 3
+ 
+//  // Write a JavaScript program that accept two integers and display the larger.
+  
+//  // check input numbers are the type of number, if not console.log('Invalid inputs)
+  
+//  // Sample numbers : 2,5
+//  // Output : 5
+//  typeof num1,num2 !== "number"
+//  var num1 = 2
+//  var num2 = 5
+ 
+//  function compareNumbers(_num1,_num2) {
+//     if (num1 < num2) {
+//         return "the larger of " + num2 ;
+//     }else {
+//         return "the larger of " + num1 ;
+//     }
+//    }
+//     var result = compareNumbers(5,2)
+//     console.log(result);
+    
+ 
+
+// // Exercise 4
+ 
+// // Write a JavaScript conditional statement to find the sign of product of three numbers.
+// // Display an console.log box with the specified sign.
+ 
+// // Sample numbers : 3, -7, 2
+// // Output : The sign is -
+//  typeof x,y,z !== "number"
+// var x = 3
+// var y =-7
+// var z = 2
+
+
+// if (x*y*z<0){alert("the sing is -.") ;
+// }
+// else
+// {
+//    alert("the sing is +.");
+   
+// }
+
+// // Exercise 5
+ 
+// // Choose the correct comparison operator to console.log true, when x is greater than y.
+// // Choose the correct comparison operator to console.log false, when x is equal to y.
+// // Choose the correct comparison operator to console.log not sure, when x is NOT equal to y.
+ 
+
+// var x = 3
+// var y = 5
+// if (typeof x,y !== "number")
+// if(x>y){
+//    console.log(true);
+   
+// }else if(x=y){
+//    console.log(false);
+   
+// }else if(x!== y){
+//    // console.log(true);
+//    console.log("true");
+   
+   
+// }
+
+// // Exercise 6
+// //   The Grade Assigner
+// // - Write a if/else statement that:
+// //   - takes 1 variable, a number score.
+// //   - returns a grade for the score, either "A", "B", "C", "D", or "F".
+// // - Call that if/else statement for a few different scores and log the result to make sure it works.
+// //
+
+// var bat = 80
+
+// if(100>=bat && 90>=bat){
+//    console.log("A");
+// }else if(80==bat ){
+//    console.log("B");
+// }else if(70<=bat ){
+//    console.log("C");
+   
+// }else if(60<=bat){
+//    console.log("D");
+   
+// }else if(59<=bat){
+//    console.log("F");
+   
+// }
+  
+// // Exercise 7 j
+ 
+// //  3) Write conditional expressions to satisfy the following safety rules:
+ 
+// //  a) If crewStatus is true, print "Crew Ready" else print "Crew Not Ready".
+ 
+// //  b) If computerStatusCode is 200, print "Please stand by. Computer is rebooting." Else if computerStatusCode is 400, print "Success! Computer online." Else print "ALERT: Computer offline!"
+ 
+// //  c) If shuttleSpeed is > 17,500, print "ALERT: Escape velocity reached!" Else if shuttleSpeed is < 8000, print "ALERT: Cannot maintain orbit!" Else print "Stable speed".
+ 
+// // Write If statement that takes number checks if the number is odd or even
+ 
+// //     if odd return "the number is odd"
+// //     if even return "the number is even"
+ 
+   
+//    const crewStatus = 6
+//    if(crewStatus == true){
+//       console.log("crew ready"); 
+//    }else {
+//       console.log("Crew Not Ready");
+      
+//    }
+//     var computerStatusCode = 100
+//     if( computerStatusCode == 200){
+//       console.log("Please stand by");
+      
+//     }
+//     else if(computerStatusCode == 400){
+//       console.log("Success! Computer online");
+      
+//     }else if(computerStatusCode !== 200 && 400){
+//       alert("Computer offline!")
+//     }
+//      var shuttleSpeed = 9000
+      
+//      if(shuttleSpeed > 17500){
+//       console.log("ALERT: Escape velocity reached!");
+      
+//      }else if(shuttleSpeed < 8000){
+//       console.log("ALERT: Cannot maintain orbit!" );
+      
+//      }else if(shuttleSpeed < 17500 && shuttleSpeed > 8000){
+//       console.log( "Stable speed");
+      
+//      }
+//      const number = prompt("enter a number: ")
+     
+//      if(number % 2 == 0){
+//       console.log("the number is even");
+      
+//      }else {
+//       console.log("the number is odd");
+      
+//      }
+
+  // Exercise 8 j
+ 
+// Write if statement that takes role and return each role with greeting
+ 
+//     if role is 'Employee' return 'Hello'
+//     if role is 'Director' return 'Greetings'
+//     if role is '' return 'Please provide role'
+//     else return 'Hi'
+   
+      // let role = "director"
+      // if(role === "employee"){
+      //    console.log("hello");
+      // }else if(role === "director"){
+      //    console.log("Greetings");
+      // }else if(role == " "){
+      //    console.log("Please provide role");
+         
+      // }else {
+      //    console.log("hi");
+         
+      // }
+
+
+  
+        // // Exercise 8 j
+ 
+// // 0. Гончигсумлай хоолны газар оров. Орсон хоолны газар нь 5000₮ - 30000₮ хооронд 15% нэмж төлдөг,
+// // бусад үед 20% - ийг нэмж төлдөг бол. Эдгээр тохиолдолуудад нийт хэдийг төлөхийг олно уу.
+// // Эдгээр тохиолдолуудад тус бүр хэдийг төлөх вэ? 3000₮, 27500₮, 100000₮.
+
+
+// let price = 100000
+// if(price>5000 && price<30000){
+//    let niitprice= price * 3/20 + price 
+//    console.log(niitprice);
+
+
+   
+// }else if(price<300000){
+//    let niitprice = price * 4/20 + price
+//    console.log(niitprice);
+   
+// }
+// function calculateTotal(amount) {
+//    let total;
+ 
+//    if (amount < 5000) {
+//      // 5000₮-ээс доош бол 20% нэмэх
+//      total = amount * 1.20;
+//    } else if (amount >= 5000 && amount <= 30000) {
+//      // 5000₮ - 30000₮ хооронд бол 15% нэмэх
+//      total = amount * 1.15;
+//    } else {
+//      // 30000₮-ээс их бол 20% нэмэх
+//      total = amount * 1.20;
+//    }
+ 
+//    return total;
+//  }
+ 
+//  // Тухай бүрийн төлбөрийг олгох
+//  console.log(calculateTotal(3000));    // Output: 3600₮ (20% нэмэгдсэн)
+//  console.log(calculateTotal(27500));  // Output: 31625₮ (15% нэмэгдсэн)
+//  console.log(calculateTotal(100000)); // Output: 120000₮ (20% нэмэгдсэн)
+        
+// Exercise 10 j
+// // Write If statement that takes string
+// //     if string length is more than 10 print the string  has more than 10 characters
+// //      if string length is more than 5 return the string has more than 5 characters
+// //       if string length is less than 1 return the string has nothing
+// //       if string length is equal to 1 return the string has 1 character
+   const Stringlength = 11
+     
+      if(Stringlength > 10){
+         console.log("The string has more than 10 characters.");
+         
+      }else if(Stringlength > 5){
+         console.log("The string has more than 5 characters.");
+         
+      }else if(Stringlength === 0){
+         console.log("The string has nothing.");
+         
+      }else if(Stringlength === 1){
+         console.log("The string has 1 character.");
+         
+      }
+   
+
+     
